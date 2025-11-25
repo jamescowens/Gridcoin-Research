53c615ee603f75379a5551018f67454576c0860a
James C. Owens <jamesowens@optonline.net>
2025-11-25 12:35:25 -0500

    Change zlib to cmake and pass compiler flags to pass with MinGW GCC 14+ compilers

--- depends/packages/zlib.mk
+++ depends/packages/zlib.mk
@@ -4,26 +4,31 @@ $(package)_download_path=https://www.zlib.net
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
 $(package)_sha256_hash=9a93b2b7dfdac77ceba5a558a580e74667dd6fede4585b91eefb60f03b72df23
 
+# We must define a build subdirectory so that 'cmake -S ..' correctly finds the source in the parent directory.
+$(package)_build_subdir = build
+
 define $(package)_set_vars
-$(package)_config_opts= CC="$($(package)_cc)"
-$(package)_config_opts+=CFLAGS="$($(package)_cflags) $($(package)_cppflags) -fPIC"
-$(package)_config_opts+=RANLIB="$($(package)_ranlib)"
-$(package)_config_opts+=AR="$($(package)_ar)"
-$(package)_config_opts_android+=CHOST=$(host)
+$(package)_config_opts=-DBUILD_SHARED_LIBS=OFF
+$(package)_config_opts+=-DCMAKE_C_FLAGS="$($(package)_cflags) $($(package)_cppflags) -fpermissive -Wno-error"
+
+ifneq ($(host_os),mingw32)
+  $(package)_config_opts+=-DCMAKE_POSITION_INDEPENDENT_CODE=ON
+endif
+
 endef
 
-# zlib has its own custom configure script that takes in options like CC,
-# CFLAGS, RANLIB, AR, and ARFLAGS from the environment rather than from
-# command-line arguments.
+# Use CMake instead of the legacy configure script
 define $(package)_config_cmds
-  env $($(package)_config_opts) ./configure --static --prefix=$(host_prefix)
+  $($(package)_cmake) -S .. -B . $($(package)_config_opts)
 endef
 
 define $(package)_build_cmds
-  $(MAKE) libz.a
+  $(MAKE) -j$(host_nproc)
 endef
 
 define $(package)_stage_cmds
-  $(MAKE) DESTDIR=$($(package)_staging_dir) install
+$(MAKE) DESTDIR=$($(package)_staging_dir) install && \
+if [ -f $($(package)_staging_dir)$(host_prefix)/lib/libzlibstatic.a ]; then \
+  cp $($(package)_staging_dir)$(host_prefix)/lib/libzlibstatic.a $($(package)_staging_dir)$(host_prefix)/lib/libz.a; \
+fi
 endef
-
